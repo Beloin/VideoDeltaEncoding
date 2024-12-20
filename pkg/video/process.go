fce50ba1b1e469c9e43e831a40121478cabe71f0
Beloin Sena <beloin.rodrigues@gmail.com>
2024-12-19 22:45:38 -0300

    Need first to load frame quantity

--- pkg/video/process.go
+++ pkg/video/process.go
@@ -13,8 +13,8 @@ func ExampleReadFrameAsBmp(inFileName string, frameNum int) io.Reader {
 	buf := bytes.NewBuffer(nil)
 	err := ffmpeg.Input(inFileName).
 		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
-    // Output("pipe:", ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "mjpeg"}). // TODO: Change to bitmap
-    Output("pipe:", ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "bmp"}).
+		// Output("pipe:", ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "mjpeg"}). // TODO: Change to bitmap
+		Output("pipe:", ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "bmp"}).
 		WithOutput(buf, os.Stdout).
 		Run()
 	if err != nil {
@@ -22,4 +22,9 @@ func ExampleReadFrameAsBmp(inFileName string, frameNum int) io.Reader {
 	}
 
 	return buf
+}
+
+// ffprobe -v error -select_streams v:0 -count_packets \
+//      -show_entries stream=nb_read_packets -of csv=p=0 input.mp4
+func GetVideoInformation(fileName string) {
 }
